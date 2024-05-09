2785a53c032515e548d67668a2c03d8059c868d8
Deepak Sharma <dsharma2828@gmail.com>
2024-05-09 15:41:35 +0530

    Project Initialization

---
+++ src/main/java/com/ecom/ecommerceapis/ECommerceApIsApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.ecommerceapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecom/ecommerceapis/ECommerceApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.ecommerceapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
